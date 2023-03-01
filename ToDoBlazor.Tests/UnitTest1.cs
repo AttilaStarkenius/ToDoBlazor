fa9efa5ebee3fbe516e12849ea6f77989b23cca9
Attila Starkenius <symphtom@hotmail.com>
2023-03-01 15:22:06 +0100

    Attila Starkenius 1.3.2023.

---
+++ ToDoBlazor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace ToDoBlazor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const int excepcted = 1;
+            const int actual = 1;
+        }
+    }
+}
